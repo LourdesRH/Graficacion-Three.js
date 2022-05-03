0a3f5254e40c7059d5b438784c1c5571ee0806d0
LourdesRH <90708386+LourdesRH@users.noreply.github.com>
2022-05-03 18:26:34 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var miRender = document.getElementById('miRender');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+//renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(miRender.clientWidth, miRender.clientHeight, true);
+
+//document.body.appendChild(renderer.domElement);
+miRender.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0xaeff32 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 4;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.04;
+    cube.rotation.y += 0.04;
+
+    renderer.render(scene, camera);
+};
+
+animate();
+
